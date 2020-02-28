aa953d6248532b55a9cb031ce893dd7e243bce12
Ghaith Darwish <ghaith.darwish01@stclairconnect.ca>
2020-02-28 16:40:41 -0500

    Created RecyclerView with static data (Add RecyclerView #3)

--- app/src/main/java/com/nextevent/Fragments/EventsFragment.java
+++ app/src/main/java/com/nextevent/Fragments/EventsFragment.java
@@ -4,19 +4,24 @@ package com.nextevent.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.nextevent.JavaBeans.CustomRecyclerviewAdapter;
+import com.nextevent.JavaBeans.EventModel;
 import com.nextevent.R;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EventsFragment extends Fragment {
-
 
     public EventsFragment() {
         // Required empty public constructor
@@ -27,7 +32,25 @@ public class EventsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events, container, false);
+        View view = inflater.inflate(R.layout.fragment_events, container, false);
+
+        ArrayList<EventModel> events = new ArrayList<>();
+        events.add(new EventModel("Ghaith", "Ghaith", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQUVJQyaDhAU2TrCTE86fMkrchtrGkGd8zmO_mFMWcZ83fGs7Oh"));
+        events.add(new EventModel("Ghaith", "Ghaith", "https://lh3.googleusercontent.com/proxy/Oz9rjqVE1WcapCGRU4ei4xZbRTf0Ca7TeZTW8mLuv4wLV_6BILgDhkcCB23ChxU5x597MLFKrCSSLNcDiFIW_12gJEUREVOG08L2bMngmrNAnNvzx7BvGkE"));
+        events.add(new EventModel("Ghaith", "Ghaith", "https://fintechbelgium.be/wp-content/uploads/2018/06/https___image.flaticon.com_icons_png_512_314_314550-480x480.png"));
+        events.add(new EventModel("Ghaith", "Ghaith", "https://i.dlpng.com/static/png/4614747_thumb.png"));
+        events.add(new EventModel("Ghaith", "Ghaith", "https://sustainablecortland00.files.wordpress.com/2013/07/event-icon.png"));
+        events.add(new EventModel("Ghaith", "Ghaith", "https://www.iconninja.com/files/618/341/273/chart-event-calendar-schedule-business-plan-month-icon.svg"));
+        events.add(new EventModel("Ghaith", "Ghaith", "https://www.transparentpng.com/thumb/calendar/red-calendar-events-icon-png-9.png"));
+        events.add(new EventModel("Ghaith", "Ghaith", "https://images.squarespace-cdn.com/content/v1/5c0887728ab7228323e0d320/1572381330660-GVXS072JMAGOCZZOHW9P/ke17ZwdGBToddI8pDm48kLxnK526YWAH1qleWz-y7AFZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVFUQAah1E2d0qOFNma4CJuw0VgyloEfPuSsyFRoaaKT76QvevUbj177dmcMs1F0H-0/BrandAlive-Icon-Inhouse2.png"));
+        events.add(new EventModel("Ghaith", "Ghaith", "https://i.dlpng.com/static/png/4614724_thumb.png"));
+
+        RecyclerView recyclerView = view.findViewById(R.id.eventRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        CustomRecyclerviewAdapter adapter = new CustomRecyclerviewAdapter(events, getActivity());
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 
 }
