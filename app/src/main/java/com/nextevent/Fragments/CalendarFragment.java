4b41796ddb9052b57ef199a9464ed76c5398ee38
AbelAnderson <Asatheabel@gmail.com>
2020-03-10 11:27:15 -0400

    #12 Added in Calendar Library

--- app/src/main/java/com/nextevent/Fragments/CalendarFragment.java
+++ app/src/main/java/com/nextevent/Fragments/CalendarFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.nextevent.R;
+import com.skyhope.eventcalenderlibrary.CalenderEvent;
 
 
 /**
@@ -27,7 +28,11 @@ public class CalendarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        CalenderEvent calendarEvent = view.findViewById(R.id.calender);
+
+        return view;
     }
 
 }
