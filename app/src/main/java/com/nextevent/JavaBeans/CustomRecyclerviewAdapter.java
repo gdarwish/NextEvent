8f605134560465e62e0d9e2e4e921b0db5348402
Ghaith Darwish <ghaith.darwish01@stclairconnect.ca>
2020-04-01 21:48:18 -0400

    colours & design #34

--- app/src/main/java/com/nextevent/JavaBeans/CustomRecyclerviewAdapter.java
+++ app/src/main/java/com/nextevent/JavaBeans/CustomRecyclerviewAdapter.java
@@ -143,7 +143,7 @@ public class CustomRecyclerviewAdapter extends RecyclerView.Adapter<CustomRecycl
                     // Creating AlertDialog when deleting an event
                     new AlertDialog.Builder(context).setTitle("Delete")
                             .setMessage("Are you sure you want to delete " + events.get(getLayoutPosition()).getTitle() + "?")
-                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .setIcon(R.drawable.ic_warning_black_24dp)
                             .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
